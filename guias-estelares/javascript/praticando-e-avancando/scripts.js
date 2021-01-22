6457d7b362d3b29babacb7211a18b4ab3efe86d8
Rodrigo Silva <rodrigonew199416@gmail.com>
2021-01-22 16:55:45 -0300

    dia 22-01-2021 4

---
+++ guias-estelares/javascript/praticando-e-avancando/scripts.js
@@ -0,0 +1,58 @@
+/*
+
+//Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(person.age)
+***************************************************************************************
+*/
+
+//Array
+
+
+/*
+const animals = {
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3
+    }
+}
+
+//acessar valores dentro do array
+console.log(animals[2].name)
+****************************************************************************************
+*/
+
+//EXERCÍCIOS
+//1
+let weight
+
+//2
+console.log(typeof weight)
+
+//3
+let person = {
+    name = 'Rodrigo',
+    age = 26,
+    weight: 9.9,
+    isSubscribed: true
+}
+
+//4
+console.log(typeof person)
+
+//5
+let student = []
+
+//6
+student = [
+    student
+]
