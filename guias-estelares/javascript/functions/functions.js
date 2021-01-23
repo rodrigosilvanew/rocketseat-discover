64fa9ec89406e4826bac3a77ade3ae813e84f1fb
Rodrigo Silva <rodrigonew199416@gmail.com>
2021-01-23 14:08:39 -0300

    dia 23-01-2021 1

---
+++ guias-estelares/javascript/functions/functions.js
@@ -0,0 +1,65 @@
+/*
+
+//function expression or anonymous function
+let total = 0
+
+//parameters
+const sum = function(number1, number2){
+    let total = number1 + number2
+    return total
+}
+
+
+let number1 = 34
+let number2 = 25
+//sum(number1, number2) //arguments
+
+//console.log(`o número 1 é ${number1}`)
+//console.log(`o número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
+console.log(total)
+
+*/
+
+
+
+
+
+
+/*
+//Função é um liquidificador
+
+function fazerSuco(fruta1, fruta2) {
+    return fruta1 + fruta2
+}
+
+const copo = fazerSuco('banana', 'maçã')
+
+
+console.log(copo)
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+//function scope
+let subject
+
+function createThink() {
+    subject = 'study'
+}
+
+console.log(subject)
+createThink()
+console.log(subject)
+
+*/
+
+
---
+++ guias-estelares/javascript/functions/functions_1.js
@@ -0,0 +1,21 @@
+//criar um aplicativo de frases motivacionais
+
+
+//declaration
+//function statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+//executar a função
+//rodar, chamar, invocar
+//execute, run, call, invoke
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('Fim do programa')
