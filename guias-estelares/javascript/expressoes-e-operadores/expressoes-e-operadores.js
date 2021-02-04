fcb4e61da5cb52562a8e3f42f8e19662ac1610bd
Rodrigo Silva <rodrigonew199416@gmail.com>
2021-02-04 18:47:31 -0300

    dia 04-02-2021 1

---
+++ guias-estelares/javascript/expressoes-e-operadores/expressoes-e-operadores.js
@@ -0,0 +1,18 @@
+/*Expressões e Operadores
+
+- Expressions
+- Operators
+    Binary
+    Unary
+    Ternary
+*/
+
+//expression
+let number
+//binary operator
+console.log(number + 1)
+//unary operator
+console.log(++number)
+console.log(typeof number)
+//ternary operator
+console.log(false ? 'alo' : 'nada')
