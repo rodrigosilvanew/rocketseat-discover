1f9606412845b85bbf6175fafaa483c72ba78455
Rodrigo Silva <rodrigonew199416@gmail.com>
2021-02-06 09:36:14 -0300

    dia 06-02-2021 1

--- guias-estelares/javascript/expressoes-e-operadores/operadores-aritmeticos.js
+++ guias-estelares/javascript/expressoes-e-operadores/operadores-aritmeticos.js
@@ -1,3 +1,29 @@
 //Operadores Aritméticos
 
-//Multiplicação
+//Multiplicação *
+console.log(3 * 5)
+
+//Divisao /
+console.log(12 / 2)
+
+//soma +
+console.log(34 + 67)
+
+//subtracao -
+console.log(34 - 23)
+
+//resto da divisao %
+reminder 11 % 3
+console.log(reminder)
+
+//incremento ++
+let increment = 0
+++increment
+console.log(increment)
+
+//decremento --
+let decrement = 0
+console.log(decrement--)
+
+//exponencial **
+console.log(3 ** 3)
