6c8fd4979ea7a18aa2a56cdb5dbc0198b50290a2
Rodrigo Silva <72414281+rodrigosilvanew@users.noreply.github.com>
2021-02-08 14:59:43 -0300

    dia 08-02-2021 2

---
+++ guias-estelares/javascript/estruturas-de-repeticao/for-of.js
@@ -0,0 +1,14 @@
+//for...of
+
+let name = 'Mayk'
+let names = ['João', 'Paulo', 'Pedro']
+
+for(let char of name) {
+    console.log(char)
+}
+
+
+
+for(let name of names) {
+    console.log(name)
+}
