95845139d2d73a846090b0fd2847184f0362b04f
Rodrigo Silva <72414281+rodrigosilvanew@users.noreply.github.com>
2021-02-10 14:57:12 -0300

    dia 10-02-2021 2

---
+++ guias-estelares/javascript/exercicios/ex04.js
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Clanson",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Augusto Cury",
+            },
+        ],
+    },
+];
+
+const totalCategories = booksByCategory.length
+
+console.log(totalCategories)
+for(let category of booksByCategory) {
+    console.log('Total de livros da categoria: ' + category.category)
+    console.log(category.books.length)
+}
+
+function countAuthors() {
+    let authors = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(authors.indexOf(book.author) == -1)
+            authors.push(book.author)
+        }
+    }
+
+
+    console.log("Total de autores: " + authors.length)
+}
+
+countAuthors();
