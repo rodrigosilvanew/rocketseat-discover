4d751b23037a6fde9f3b3ae5dd04cbefdaa869b1
Rodrigo Silva <72414281+rodrigosilvanew@users.noreply.github.com>
2021-02-16 17:39:39 -0300

    dia 16-02-2021 12

---
+++ preparacao-de-astronautas/pilotando-com-a-dom/introducao/dom.md
@@ -0,0 +1,10 @@
+# DOM 
+* É o HTML convertido para um objeto JavaScript
+* API que representa e interage com HTML
+* Estrutura de dados do tipo árvore, criada pelo browser
+* Propriedades e métodos
+
+# Para que?
+* JavaScript usa a DOM para se conectar ao HTML
+* Manipular o HTML com o JavaScript
+* Você só programa para WEB porque existe a DOM
---
+++ preparacao-de-astronautas/pilotando-com-a-dom/selecionando-elementos/script.js
@@ -0,0 +1,44 @@
+/*
+// getElementById()
+const element = document.getElementById('blog-title')
+console.log(element)
+*/
+
+
+
+/*
+//getElementsByClassName()
+const element = document.getElementsByClassName('one')
+console.log(element)
+*/
+
+
+/*
+//getElementsByTagName
+const element = document.getElementsByTagName('h1')
+console.log(element)
+*/
+
+
+/*
+//querySelector()
+const element = document.querySelector('.one')
+console.log(element)
+*/
+
+
+/*
+//querySelectorAll()
+const elements = document.querySelectorAll('.one')
+elements.forEach(el => console.log(el))
+*/
+
+
+
+
+//Qual usar????
+//getElementById (elment)
+//getElementsByClassName (HTMLcollection)
+//getElementsByTagName (HTMLcollection)
+//querySelector(element)
+//querySelectorAll(Nodelist)
