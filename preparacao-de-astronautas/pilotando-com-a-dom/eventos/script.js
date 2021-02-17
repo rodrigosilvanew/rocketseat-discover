800acbbf2c9fad7dd6bf26a44fde1cff4472de43
Rodrigo Silva <72414281+rodrigosilvanew@users.noreply.github.com>
2021-02-17 15:15:41 -0300

    dia 17-02-2021 5

--- preparacao-de-astronautas/pilotando-com-a-dom/eventos/script.js
+++ preparacao-de-astronautas/pilotando-com-a-dom/eventos/script.js
@@ -1,1 +1,18 @@
 //Eventos
+/*
+const h1 = document.querySelector('h1')
+h1.addEventListener('click', print)
+
+function print() {
+    console.log('print')
+}
+*/
+
+
+//Argumento event
+
+const input = document.querySelector('input')
+
+input.onkeydown = function(event) {
+    console.log(event.key)
+}
