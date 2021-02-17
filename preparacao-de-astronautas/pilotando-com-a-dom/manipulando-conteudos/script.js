1ec06b71491c73a71a33f533cb96647548767678
Rodrigo Silva <72414281+rodrigosilvanew@users.noreply.github.com>
2021-02-17 11:51:23 -0300

    dia 17-02-2021 1

--- preparacao-de-astronautas/pilotando-com-a-dom/manipulando-conteudos/script.js
+++ preparacao-de-astronautas/pilotando-com-a-dom/manipulando-conteudos/script.js
@@ -1,5 +1,6 @@
-/*
 //Manipulando conteúdo
+
+/*
 //textContent
 const element = document.querySelector('h1')
 element.textContent += ' Olá, Devs!'
@@ -7,3 +8,36 @@ console.log(element.textContent)
 */
 
 
+/*
+//Manipulando texto
+//innerText
+const element = document.querySelector('h1')
+element.innerText = 'Olá, Devs!'
+*/
+
+
+/*
+//innerHTML
+const element = document.querySelector('h1')
+element.innerHTML = 'Olá, Devs! <small>!!!</small>'
+*/
+
+
+/*
+//value
+const element = document.querySelector('input')
+element.value = 'Valor que eu quiser'
+*/
+
+
+//Atributos
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+const headerID = document.querySelector('#header')
+
+console.log(headerID.getAttribute('class'))
+
+header.removeAttribute('id')
+
+header.setAttribute('class', 'bg header')
